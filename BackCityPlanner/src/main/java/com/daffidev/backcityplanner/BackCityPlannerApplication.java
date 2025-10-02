13f31b8c4dadc68246c28df191291c62923a5f75
Daffi <devstuff@bydaffi.com>
2025-10-02 00:49:39 -0600

    Init de este pdo, los amo equipo

---
+++ BackCityPlanner/src/main/java/com/daffidev/backcityplanner/BackCityPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.daffidev.backcityplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCityPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackCityPlannerApplication.class, args);
+    }
+
+}
---
+++ BackCityPlanner/src/test/java/com/daffidev/backcityplanner/BackCityPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daffidev.backcityplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCityPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
