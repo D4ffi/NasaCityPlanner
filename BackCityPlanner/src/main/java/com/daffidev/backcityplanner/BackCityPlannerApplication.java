bbe1602f8b86e8cf376d0c9c6bd99abd1a2fdfda
Daffi <devstuff@bydaffi.com>
2025-10-02 01:11:17 -0600

    Agregué el Logger oficial del canal

--- BackCityPlanner/src/main/java/com/daffidev/backcityplanner/BackCityPlannerApplication.java
+++ BackCityPlanner/src/main/java/com/daffidev/backcityplanner/BackCityPlannerApplication.java
@@ -1,13 +1,18 @@
 package com.daffidev.backcityplanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackCityPlannerApplication {
 
+    public static final Logger WAKOLOGGER = LoggerFactory.getLogger(BackCityPlannerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(BackCityPlannerApplication.class, args);
+
     }
 
 }
