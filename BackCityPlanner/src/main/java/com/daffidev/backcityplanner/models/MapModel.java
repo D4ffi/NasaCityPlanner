462c52f78b6888d53ddd7786c17fd2218019c27b
AndresTrujo <202360022@ucc.mx>
2025-10-04 14:40:58 -0600

    Arquitectura MVC init

---
+++ BackCityPlanner/src/main/java/com/daffidev/backcityplanner/models/MapModel.java
@@ -0,0 +1,8 @@
+package com.daffidev.backcityplanner.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class MapModel {
+    
+}
---
+++ BackCityPlanner/src/main/java/com/daffidev/backcityplanner/repositories/IMapRepository.java
@@ -0,0 +1,9 @@
+package com.daffidev.backcityplanner.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMapRepository{
+
+    
+}
